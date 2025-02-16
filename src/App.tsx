ed51d7799ee4939c8ecbd1b39dee633a09b732cf
Ralph Largo <devlargs@gmail.com>
2025-02-16 18:01:51 +0800

    feat: created simple layout to create drone control

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,72 @@
-import {Flex, MantineProvider} from '@mantine/core';
+import {Button, Flex, MantineProvider, Text} from '@mantine/core';
 import '@mantine/core/styles.css';
+import {useState} from 'react';
 
 function App() {
+    const [instructionSet, setInstructionSet] = useState<string[]>([]);
+
+    const addInstruction = (instruction: string) => {
+        if (instruction === '') {
+            setInstructionSet([]);
+            return;
+        }
+
+        setInstructionSet([...instructionSet, instruction]);
+    };
+
     return (
         <MantineProvider>
             <Flex p="xl">
-                <div>hehe</div>
+                <Flex direction="column" gap="md">
+                    <h1>Drone Challenge</h1>
+                    <Text aria-live="polite">
+                        {instructionSet.length ? `Instruction Set: ${instructionSet.join('')}` : ''}
+                    </Text>
+                    <Flex gap="md">
+                        <Button
+                            onClick={() => addInstruction('x')}
+                            aria-label="Take Photo"
+                            color="green"
+                        >
+                            📸 Take Photo
+                        </Button>
+                        <Button
+                            onClick={() => addInstruction('^')}
+                            aria-label="Move Up"
+                            color="green"
+                        >
+                            ⬆️ Move Up
+                        </Button>
+                        <Button
+                            onClick={() => addInstruction('v')}
+                            aria-label="Move Down"
+                            color="green"
+                        >
+                            ⬇️ Move Down
+                        </Button>
+                        <Button
+                            onClick={() => addInstruction('<')}
+                            aria-label="Move Left"
+                            color="green"
+                        >
+                            ⬅️ Move Left
+                        </Button>
+                        <Button
+                            onClick={() => addInstruction('>')}
+                            aria-label="Move Right"
+                            color="green"
+                        >
+                            ➡️ Move Right
+                        </Button>
+                        <Button
+                            onClick={() => addInstruction('')}
+                            aria-label="Reset Instructions"
+                            color="green"
+                        >
+                            🔄 Reset
+                        </Button>
+                    </Flex>
+                </Flex>
             </Flex>
         </MantineProvider>
     );
