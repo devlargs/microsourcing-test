8c051c3dbb51d3f9a618cb55c2ccf5e2278109a4
Ralph Largo <devlargs@gmail.com>
2025-02-17 07:30:18 +0800

    feat: created vercel.json file

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
-import express from 'express';
 import cors from 'cors';
-import instructDrone from './routes/instruct-drone';
+import express from 'express';
 import getBillboard from './routes/get-billboard';
+import instructDrone from './routes/instruct-drone';
 
 const app = express();
 
@@ -11,3 +11,5 @@ app.get('/instruct-drone', instructDrone);
 app.get('/get-billboard', getBillboard);
 
 app.listen(4001, () => console.log(`Api started at http://localhost:4001`));
+
+export default app;
